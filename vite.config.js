18ce2a936f1f34f38585ef98ce86a1701951a5a9
Artiqbaeva <artiqbaevamafruza@gmail.com>
2025-07-15 21:47:54 +0500

    pwa added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), 
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'My Tasks PWA',
+        short_name: 'Tasks',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#3b82f6', // Tailwind cyan-500
+        icons: [
+          {
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
